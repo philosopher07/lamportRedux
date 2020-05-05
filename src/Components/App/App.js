f2c23748f32909c6a4264fa839af312ad1c466d9
Dev Jana <devjana@Devs-MBP-2.domain>
2020-05-05 15:10:08 -0500

    basic setup

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>App.js</h2>
+        </header>
+      </div>
+    ); // end return
+  } //end render
+} // end component
+
+export default App;
