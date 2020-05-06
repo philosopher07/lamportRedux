36bd0b7ce8d61f606ce0a5834b7c138352c8c8e8
Dev Jana <devjana@Devs-MBP-2.domain>
2020-05-06 10:51:24 -0500

    ready for day 2 lecture

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends React.Component{
+
+  clickHandler = () =>{
+    console.log( 'in clickHandler' );
+    this.props.dispatch( { type: 'click', asdf: 'thingy' } );
+  } // end clickHandler
+
+  unclickHandler = () =>{
+    console.log( 'in unclickHandler' );
+    this.props.dispatch( { type: 'unclick' } );
+  } // end clickHandler
+
+  tacoClickHandler = () =>{
+    console.log( 'in unclickHandler' );
+    this.props.dispatch( { type: 'taco' } );
+  } // end clickHandler
+
+  componentDidMount(){
+    console.log( 'in componentDidMount, props:', this.props );
+  } // end componentDidMount
+
   render() {
+    console.log( this.props );
     return (
       <div className="App">
         <header className="App-header">
           <h2>App.js</h2>
+          <p>Clicks: { this.props.reduxState }</p>
         </header>
+        <button onClick={ this.clickHandler }>Click Me</button><button onClick={ this.unclickHandler }>Unclick Me</button><button onClick={ this.tacoClickHandler }>I'm Hungry</button>
       </div>
     ); // end return
   } //end render
 } // end component
 
-export default App;
+const putReduxStateOnProps = ( reduxState ) => ( { reduxState } );
+
+export default connect( putReduxStateOnProps )(App);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
+// for redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// initial states
+const firstReducerInitialState = 0;
+
+const firstReducer = ( state = firstReducerInitialState, action ) =>{
+  console.log( 'in firstReducer:', 'state:', state, 'action:', action );
+  if( action.type === 'click' ){
+    return state + 1;
+  }
+  else if( action.type === 'unclick' ){
+    return state - 1;
+  }
+  else if( action.type === 'taco' ){
+    return state + 10;
+  }
+  return state;
+} // firstReducer
+
+// create a storage space for redux data, tell it to use our reducer
+const storeInstance = createStore( firstReducer );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ storeInstance }><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
